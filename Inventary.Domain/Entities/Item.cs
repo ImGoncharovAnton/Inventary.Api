513193ef6a22b5f264a489935e1dd195b5879474
Anton <gold-library@yandex.ru>
2022-07-05 09:21:02 +0300

    add migration items table and change field ID Guid to int type

--- Inventary.Domain/Entities/Item.cs
+++ Inventary.Domain/Entities/Item.cs
@@ -2,8 +2,11 @@
 
 namespace Inventary.Domain.Entities;
 
-public class Item: BaseEntity
+public class Item
 {
+    public int Id { get; set; }
+    public DateTime CreatedDate { get; set; } = DateTime.UtcNow;
+    public DateTime UpdateDate { get; set; } = DateTime.UtcNow;
     public string ItemName { get; set; }
     public StatusEnum.StatusType Status { get; set; }
     public decimal Price { get; set; }
---
+++ Inventary.Repositories/Migrations/20220705061850_return_table_items.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Inventary.Repositories.Migrations
+{
+    public partial class return_table_items : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Item_Rooms_RoomId",
+                table: "Item");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_ItemSetup_Item_ItemsId",
+                table: "ItemSetup");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Item",
+                table: "Item");
+
+            migrationBuilder.RenameTable(
+                name: "Item",
+                newName: "Items");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Item_RoomId",
+                table: "Items",
+                newName: "IX_Items_RoomId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Items",
+                table: "Items",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Items_Rooms_RoomId",
+                table: "Items",
+                column: "RoomId",
+                principalTable: "Rooms",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ItemSetup_Items_ItemsId",
+                table: "ItemSetup",
+                column: "ItemsId",
+                principalTable: "Items",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Items_Rooms_RoomId",
+                table: "Items");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_ItemSetup_Items_ItemsId",
+                table: "ItemSetup");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Items",
+                table: "Items");
+
+            migrationBuilder.RenameTable(
+                name: "Items",
+                newName: "Item");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Items_RoomId",
+                table: "Item",
+                newName: "IX_Item_RoomId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Item",
+                table: "Item",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Item_Rooms_RoomId",
+                table: "Item",
+                column: "RoomId",
+                principalTable: "Rooms",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ItemSetup_Item_ItemsId",
+                table: "ItemSetup",
+                column: "ItemsId",
+                principalTable: "Item",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
